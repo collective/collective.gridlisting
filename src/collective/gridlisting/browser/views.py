a59d720d6b081430a75e5ef557bb330b73f8a4ca
Peter Mathis <peter.mathis@kombinat.at>
2023-06-22 08:54:29 +0200

    fix view class for collection

--- src/collective/gridlisting/browser/views.py
+++ src/collective/gridlisting/browser/views.py
@@ -1,5 +1,10 @@
+from plone.app.contenttypes.browser.collection import CollectionView
 from plone.app.contenttypes.browser.folder import FolderView
 
 
-class GridListing(FolderView):
+class FolderGridListing(FolderView):
+    pass
+
+
+class CollectionGridListing(CollectionView):
     pass
