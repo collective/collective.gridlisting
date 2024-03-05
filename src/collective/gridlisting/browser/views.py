feb23ffd4abbb302cc374d3bf9e4016eccb1166f
Peter Mathis <peter.mathis@kombinat.at>
2024-03-05 10:55:14 +0100

    Add gridlisting template for contentlisting tile

--- src/collective/gridlisting/browser/views.py
+++ src/collective/gridlisting/browser/views.py
@@ -1,6 +1,16 @@
 from plone.app.contenttypes.browser.collection import CollectionView
 from plone.app.contenttypes.browser.folder import FolderView
 
+try:
+    from plone.app.standardtiles.contentlisting import ContentListingTile
+    from plone.event.interfaces import IEvent
+    from zope.component import getMultiAdapter
+    from zope.contentprovider.interfaces import IContentProvider
+
+    HAS_PAS = True
+except ImportError:
+    HAS_PAS = False
+
 
 class FolderGridListing(FolderView):
     pass
@@ -8,3 +18,17 @@ class FolderGridListing(FolderView):
 
 class CollectionGridListing(CollectionView):
     pass
+
+
+if HAS_PAS:
+    class ContentlistingTileGridListing(ContentListingTile):
+        def is_event(self, obj):
+            if getattr(obj, "getObject", False):
+                obj = obj.getObject()
+            return IEvent.providedBy(obj)
+
+        def formatted_date(self, item):
+            provider = getMultiAdapter(
+                (self.context, self.request, self), IContentProvider, name="formatted_date"
+            )
+            return provider(item)
