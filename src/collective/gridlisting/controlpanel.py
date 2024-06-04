af47a0ecc6ec5d05b8ac84a89797b22341928c11
Peter Mathis <peter.mathis@kombinat.at>
2024-06-04 12:52:30 +0200

    default values from registry

--- src/collective/gridlisting/controlpanel.py
+++ src/collective/gridlisting/controlpanel.py
@@ -12,6 +12,7 @@ class IGridListingControlPanel(Interface):
         title=_("Container row"),
         description=_("eg. if you want to set gutter between columns define here."),
         required=False,
+        default="",
     )
 
     column_css_class = schema.TextLine(
@@ -20,6 +21,7 @@ class IGridListingControlPanel(Interface):
             "Use grid css class combinations for column. Example: 'col-12 col-md-6 col-xl-3'"
         ),
         required=False,
+        default="",
     )
 
     column_content_css_class = schema.TextLine(
@@ -48,12 +50,15 @@ class IGridListingControlPanel(Interface):
     item_title_tag = schema.Choice(
         title=_("Listing item title tag"),
         values=LISTING_TITLE_TAGS,
+        required=False,
         default="h3",
     )
 
     preview_scale = schema.Choice(
         title=_("Preview image scale"),
         vocabulary="plone.app.vocabularies.ImagesScales",
+        required=False,
+        default="preview",
     )
 
     enable_masonry = schema.Bool(
@@ -69,6 +74,25 @@ class IGridListingControlPanel(Interface):
             'Options for "pat-masonry" see https://patternslib.com/demos/masonry.'
         ),
         required=False,
+        default="",
+    )
+
+    show_more_link = schema.Bool(
+        title=_("Show more link"),
+        description=_(
+            "Show a separate link to the item below the description/title with the given text below. "
+            "If deactivated, the item tile is used as link."
+        ),
+        required=False,
+        default=False,
+        missing_value=False,
+    )
+
+    more_link_text = schema.TextLine(
+        title=_("Text for 'more' link below title/description"),
+        required=False,
+        default="more",
+        missing_value="more",
     )
 
 
