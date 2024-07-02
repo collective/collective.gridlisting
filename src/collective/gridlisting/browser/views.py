18750f0c0cab2327fa74e5a8ac34ddd7ce7d4f19
Peter Mathis <peter.mathis@kombinat.at>
2024-07-02 10:12:54 +0200

    Fix `preview_scale` value for `listing_view` template

--- src/collective/gridlisting/browser/views.py
+++ src/collective/gridlisting/browser/views.py
@@ -3,6 +3,7 @@ from plone import api
 from plone.app.contenttypes.browser.collection import CollectionView
 from plone.app.contenttypes.browser.folder import FolderView
 from plone.dexterity.browser.view import DefaultView
+from plone.memoize.view import memoize
 
 
 class GridListingBase(DefaultView):
@@ -23,6 +24,17 @@ class GridListingBase(DefaultView):
         return api.portal.get_registry_record(
             "plone.allow_anon_views_about", default=False
         )
+
+    @memoize
+    def get_thumb_scale_summary(self):
+        if getattr(self.context, "suppress_thumbs", False):
+            return None
+        thsize = getattr(self.context, "thumb_scale_summary", None)
+        if thsize:
+            return thsize
+        if api.portal.get_registry_record("plone.no_thumbs_summary", default=False):
+            return None
+        return self.get_default("preview_scale")
 
 
 class FolderGridListing(GridListingBase, FolderView):
